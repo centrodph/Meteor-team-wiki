afcf53cf4bd32f5988504879d073350d74f6feeb
GerW <centrodph@gmail.com>
2017-08-01 10:36:50 -0300

    moved diagram

--- server/main.js
+++ server/main.js
@@ -10,22 +10,5 @@ Meteor.startup(() => {
   });
   Meteor.publish('usernames',function(ids){
     return Meteor.users.find({_id : {$in:ids}});
-  })
-
-
-
-  //process.env.MAIL_URL = "smtp://codigo1888:()U`XnW~*8\hk>~9@smtp.gmail.com:587";
-
-
-  //()U`XnW~*8\hk>~9
-
-  // Email.send({
-  //   to: "centrodph@gmail.com",
-  //   from: "codigo1888@gmail.com",
-  //   subject: "Example Email",
-  //   text: "The contents of our email in plain text.",
-  // });
-
-
-
+  });
 });
